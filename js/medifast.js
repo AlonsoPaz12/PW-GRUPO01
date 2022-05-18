13188354bb60a50787eda6a6929317518909dc9d
20191288 <20191288@aloe.ulima.edu.pe>
2022-05-18 01:46:05 -0500

    tarjeta

--- js/medifast.js
+++ js/medifast.js
@@ -2,10 +2,10 @@ function register() {
     const radioButton = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
         switch (radioButton) {
             case "option1":
-                location.href = "2pac.html";
+                document.getElementById("signInForm").action = "inicio_paciente.html";
                 break;
             case "option2":
-                location.href = "med.html";
+                document.getElementById("signInForm").action = "inicio_medico.html";
                 break;
             default:
                 location.href
